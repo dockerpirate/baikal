c10434822104a03393dc5c10acd1fc885727835b
dockerpirate <52665720+dockerpirate@users.noreply.github.com>
2020-06-11 22:57:19 +0200

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -7,7 +7,7 @@ if [ "$TRAVIS_PULL_REQUEST" = "true" ] || [ "$TRAVIS_BRANCH" != "master" ]; then
     .
   exit $?
 fi
-SHA="$(shell git rev-parse --short HEAD)"
+SHA="${shell git rev-parse --short HEAD}"
 echo $DOCKER_PASSWORD | docker login -u dockerpirate --password-stdin &> /dev/null
 TAG="${SHA}"
 docker buildx build \
